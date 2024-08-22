f8769e08e1248a7496f75226e4d963a58d2f35fa
Eclipse175 <fahimkhobar2004@gmail.com>
2024-08-22 03:18:58 +0300

    added a modal, alot of fixes yet to be made

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const hamburger = document.querySelector('.hamburger');
 const navUl = document.querySelector('nav ul');
-const carSearch = document.querySelector('input');
+const carSearch = document.getElementById('search-car');
+const detailsButtons = document.querySelectorAll('.product button');
+console.log(detailsButtons);
 
 hamburger.addEventListener('click', function () {
+
     navUl.classList.toggle('show');
     hamburger.classList.toggle('active');
 });
 
 const carNames = document.querySelectorAll('.names'); // Assuming each car has a class 'car-name'
-console.log(carNames);
-carSearch.addEventListener('input', function() {
+carSearch.addEventListener('input', function () {
     const searchTerm = this.value.toLowerCase();
-    
+
     carNames.forEach(name => {
         const carName = name.textContent.toLowerCase();
-        const carCard = name.closest('.product'); // Assuming each car is in a 'car-card' container
-        
+        const carCard = name.closest('.product'); // Assuming each car is in a '.product' container
+
         if (carName.includes(searchTerm)) {
             carCard.style.display = 'block';
         } else {
@@ -23,3 +25,12 @@ carSearch.addEventListener('input', function() {
         }
     });
 });
+
+detailsButtons.forEach(button => {
+    button.addEventListener('click', toggleModalOne());
+});
+
+const modalOne = document.getElementById('modal-one');
+const modalOneContent = document.getElementById('content-one');
+const closeModal = document.querySelector('.close-button');
+console.log(closeModal);
