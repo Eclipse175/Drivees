cf744d97718d4410520128864c28138e9bee2578
Eclipse175 <fahimkhobar2004@gmail.com>
2024-08-24 10:53:41 +0300

    added a functional modal, will style it better later

--- app.js
+++ app.js
@@ -2,35 +2,38 @@ const hamburger = document.querySelector('.hamburger');
 const navUl = document.querySelector('nav ul');
 const carSearch = document.getElementById('search-car');
 const detailsButtons = document.querySelectorAll('.product button');
-console.log(detailsButtons);
+const modalOne = document.getElementById('modal-one');
+const modalOneContent = document.getElementById('content-one');
+const closeButton = document.getElementById('close');
 
 hamburger.addEventListener('click', function () {
-
     navUl.classList.toggle('show');
     hamburger.classList.toggle('active');
 });
 
-const carNames = document.querySelectorAll('.names'); // Assuming each car has a class 'car-name'
-carSearch.addEventListener('input', function () {
-    const searchTerm = this.value.toLowerCase();
 
-    carNames.forEach(name => {
-        const carName = name.textContent.toLowerCase();
-        const carCard = name.closest('.product'); // Assuming each car is in a '.product' container
-
-        if (carName.includes(searchTerm)) {
-            carCard.style.display = 'block';
-        } else {
-            carCard.style.display = 'none';
-        }
+detailsButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        modalOne.style.display = 'flex';
     });
 });
 
-detailsButtons.forEach(button => {
-    button.addEventListener('click', toggleModalOne());
+closeButton.addEventListener('click', function () {
+    modalOne.style.display = 'none';
 });
 
-const modalOne = document.getElementById('modal-one');
-const modalOneContent = document.getElementById('content-one');
-const closeModal = document.querySelector('.close-button');
-console.log(closeModal);
+// const carNames = document.querySelectorAll('.names'); // Assuming each car has a class 'car-name'
+// carSearch.addEventListener('input', function () {
+//     const searchTerm = this.value.toLowerCase();
+
+//     carNames.forEach(name => {
+//         const carName = name.textContent.toLowerCase();
+//         const carCard = name.closest('.product'); // Assuming each car is in a '.product' container
+
+//         if (carName.includes(searchTerm)) {
+//             carCard.style.display = 'block';
+//         } else {
+//             carCard.style.display = 'none';
+//         }
+//     });
+// });
